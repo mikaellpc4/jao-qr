724e35a3eca01f78ddc45712af3e23c82af0a752
mikaellpc <60710335+mikaellpc4@users.noreply.github.com>
2023-03-17 17:23:03 -0300

    Feat: Input Styles finishied

--- src/pages/components/input/index.tsx
+++ src/pages/components/input/index.tsx
@@ -2,8 +2,16 @@ import type { InputProps } from "./props";
 
 export function Input({ name, error, register, ...rest }: InputProps) {
   return (
-    <div>
-      <input type="text" {...rest} {...register(name)} />
+    <div className="flex w-full">
+      <span className="flex h-12 w-[25%] items-center justify-center rounded-l-md border-[1px] border-gray-400 bg-gray-300 font-semibold text-black/60 shadow-lg">
+        {name}
+      </span>
+      <input
+        className="w-[80%] rounded-r-md border-[1px] border-gray-400 pl-4 font-semibold"
+        type="text"
+        {...rest}
+        {...register(name)}
+      />
       <span>{error}</span>
     </div>
   );
